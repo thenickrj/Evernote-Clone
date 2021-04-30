9e3a2ba8cac7f36d7659f0061432a5b8e61d80e5
Nikhil Singh <nikhilbangalor@gmail.com>
2021-04-30 11:16:27 +0530

    commit

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAh_Smo9PYHWDwnr3-34aIegGX16yy2Of8",
+  authDomain: "evernote-clone-ee7a8.firebaseapp.com",
+  projectId: "evernote-clone-ee7a8",
+  storageBucket: "evernote-clone-ee7a8.appspot.com",
+  messagingSenderId: "199086335709",
+  appId: "1:199086335709:web:6a4d424b6e06fad3dc5d34",
+};
+
+export default firebaseConfig;
